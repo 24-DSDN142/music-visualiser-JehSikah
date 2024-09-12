4effee47b6ffe80f799042c2b68742ae7e735118
JehSikah <jess.tia.merriman@gmail.com>
2024-09-12 21:05:28 +1200

    credits and zooms

--- music_starter.js
+++ music_starter.js
@@ -10,31 +10,25 @@ let static;
 let vignette;
 
 //timing variables
-let countMax = 20456; //end of song
-let firstBeat = 149; //first beat of song
+let countMax = 20458; //end of song
 
 let chor1 = [812, 2545];
-let chor2 = [5290, 7004];
-let chor3 = [9747, 15000];
-
-let crash = [65, 84, 106, 127]; 
-
-let timer = 0;
-let beat = 0;
+let chor2 = [5250, 7004];
+let chor3 = [9747, 14000]; //13725ish 13580
+let crash = [65, 84, 106, 127]; //intro crash cymbal times
 
 //other
-
-let faceOffset = 30;
-
 let barWidth = 15;
 let barSpace = barWidth + 5;
+
+let basVol = 63;
 
 // vocal, drum, bass, and other are volumes ranging from 0 to 100
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
   rectMode(CENTER);
   imageMode(CENTER);
+  textAlign(CENTER, CENTER);
   noStroke();
-  textFont('Brush Script MT'); // please use CSS safe fonts
   
   if (firstRun) {
     eyeAng = loadImage("images/eyeAng.png");
@@ -47,16 +41,13 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
     firstRun = false;
   }
 
-  let othVol = 70;
-  let basVol = 63;
-
   //center everything so I can build around (0, 0).  
+  let frameMap = map (bass, 0, 100, 1, 1.05);
   push();
   translate(width/2, height/2);
+  scale(frameMap);
 
   //plays intro sequence then main song
-  let oop = 60
-
   if (counter <= crash[0]) {
     introscene();
     makevignette();
@@ -79,28 +70,38 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
     pop();
     makevignette();
   } else {
-    background(40);
+    background(20);
 
     //flashes red to bass
-    if (bass >= basVol) {
+    if (bass >= basVol && counter > chor1[1]) {
     background(100, 0, 0);
      } 
 
     //set sound waves during chorus, face during verse
-    
     if (counter <= chor1[1] || counter >= chor2[0] && counter <= chor2[1] || counter >= chor3[0]) {
-      let waveList = [vocal, drum, other];
-      let moveList = [];
-      let resizeList = [];
-      let displaceList = [];
-      let wave, move, resize, displace;
-      if (counter <= chor2[1]) {
-        
+      let soundMap = map(vocal, 0, 100, -30, 20);
+      let scaleMap = map(vocal, 0, 100, 0.6, 1.4);
+      if (counter <= chor1[1]) {
+        graph(vocal, 0, 1, 80);
+        graph(bass, other, 1.2, 220);
+        graph(other, drum, 2.5, -320);
+        graph(drum, bass, 1.7, -140)
+      } else if (counter >= chor2[0] && counter <= chor2[1]) {
+        push();
+        translate(0, soundMap)
+        graph(vocal, 0, 1, 340);
+        graph(drum, drum, 1.5, 160);
+        graph(other, drum, 2.5, -320);
+        pop();
+      } else if (counter >= chor3[0]) {
+        push();
+        scale(scaleMap);
+        translate(0, soundMap * 1.5);
+        graph(vocal, -drum, 1, 340);
+        graph(drum, drum, 1.5, 160);
+        graph(other, drum, 2.5, -320); 
+        pop();
       }
-
-      graph(vocal, 0, 1, 340);
-      graph(drum, drum, 1.5, 160);
-      graph(other, drum, 2.5, -320);
 
       //fades from static to visualiser.
       if (counter < chor1[0]) {
@@ -110,35 +111,107 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
       }
 
     } else if (counter > chor1[1] && counter < chor2[0] || counter > chor2[1] && counter < chor3[0]) {
-      verse(words, vocal, drum, counter);
+      let zoom;
+      let zoomMap;
+      if (counter > chor1[1] && counter < chor1[1] + 100) {
+        //zoom in verse 1
+        zoom = chor1[1] + 100 - counter;
+        zoomMap = map(zoom, 100, 0, 0, 1);
+        push();
+        scale(zoomMap);
+        face(words, vocal, drum, counter, eyeNorm);
+        pop();
+      } else if (counter > chor1[1] && counter < chor2[0]) {
+        //verse 1
+        face(words, vocal, drum, counter, eyeNorm);
+
+      } else if (counter > chor2[1] && counter < chor3[0] - 100) {
+        //verse 2
+        face(words, vocal, drum, counter, eyeAng);
+
+      } else if (counter >= chor3[0] - 100 && counter < chor3[0]) {
+        //zoom out verse 2
+        zoom = chor3[0] - counter;
+        zoomMap = map(zoom, 100, 0, 1, 0);
+        push();
+        scale(zoomMap);
+        face(words, vocal, drum, counter, eyeAng);
+        pop();
+      } 
     }
-    
     frame();
 
-    if (bass >= basVol) {
-
+    //flashes red to bass over all elements
+    if (bass >= basVol && counter > chor1[1]) {
       fill(200, 0, 0, 40);
       rect(0, 0, width, height);
     } 
   
+    //screen gets redder over time
     overlay(counter);
 
-    //end credit 
+    //end credit = counter > chor3[1]
+    let outro = countMax - chor3[1];
+    let outroBeat = outro / 10;
+    let fadeOut = map(counter, chor3[1], chor3[1] + 1000, 0, 255);
     if (counter > chor3[1]) {
-      let fadeOut = map(counter, chor3[1], chor3[1] + 500, 0, 255);
       fill(0, fadeOut);
       rect(0, 0, width, height);
 
-      if (fadeOut == 255) {
+      let title = 100;
+      let sub = 70;
+      let suber = 50;
+
+      if (fadeOut >= 260) {
+        textFont('Verdana'); // please use CSS safe fonts
+        if (bass >= basVol && counter > chor1[1]) {
+          strokeWeight (5);
+          stroke(200, 0, 0);
+        } else {
+          noStroke;
+        }
         fill(255);
-        text("Clint Eastwood", 0, 0);
+
+        if (counter < outroBeat*2 + chor3[1]) {
+          credittext('Clint Eastwood', title, -50);
+          credittext('By Gorillaz', sub, 50);
+        } else if (counter < outroBeat*3 + chor3[1]) {
+          credittext('Music Visualiser', sub, -50);
+          credittext('By Jess', suber, 50);
+        } else if (counter < outroBeat*4 + chor3[1]) {
+          credittext('Honourable Mentions', sub, -50);
+          credittext('Hunter', title, 50);
+        } else if (counter < outroBeat*5 + chor3[1]) {
+          credittext('Honourable Mentions', sub, -50);
+          credittext('Phoebe', title, 50);
+        } else if (counter < outroBeat*6 + chor3[1]) {
+          credittext('Honourable Mentions', sub, -50);
+          credittext("Phoebe's Rats", sub, 50);
+        } else if (counter < outroBeat*7 + chor3[1]) {
+          credittext('Honourable Mentions', sub, -50);
+          credittext("My Mum & Dad", sub, 50);
+        } else if (counter >= outroBeat*7 + chor3[1]) {
+          credittext('Honourable Mentions', sub, -50);
+          credittext("A bird I saw once", sub, 50);
+        } else if (counter >= outroBeat*8 + chor3[1]) {
+          credittext('Honourable Mentions', sub, -50);
+          credittext("Noah the dog", sub, 50);
+        } else if (counter >= outroBeat*9 + chor3[1]) {
+          credittext('Honourable Mentions', sub, -50);
+          credittext("and You", title, 50);
+        }
+        
       }
     }
 
+    console.log(counter);
   }
- 
   pop();
+}
 
+function credittext(message, size, move) {
+  textSize(size);
+  text(message, 0, move);
 }
 
 function introscene() {
@@ -172,7 +245,7 @@ function introscene() {
 }
 
 function makevignette() {
-  tint(255, 100);
+  tint(255, 150);
   image(vignette, 0, 0, 960, 540);
 }
 
@@ -181,7 +254,7 @@ function graph(wave, move, resize, displace) {
   if (move == 0) {
     barMove = 0;
   } else {
-    barMove = map(move, 0, 100, -5, 5);
+    barMove = map(move, 0, 100, -7, 7);
   }
   
   push();
@@ -190,14 +263,12 @@ function graph(wave, move, resize, displace) {
   pop();
 }
 
-function verse(words, vocal, drum, counter) {
+function face(words, vocal, drum, counter, eyetype) {
   push();
-  scale(0.9); //fit face in screen while spinning
-  //rotate(counter);
-
+  scale(0.9);
   bob = map(vocal, 0, 100, -30, 30);
   translate(0, bob);
-  imagetry(words, drum, counter);
+  imagetry(words, drum, counter, eyetype);
   pop();
 }
 
@@ -210,6 +281,7 @@ function overlay(counter) {
 function frame() {
   let frameThicc = 80;
   let frameShad = frameThicc + 20;
+  let excess = 50;
   let frameCol;
   let frameEdge;
 
@@ -222,32 +294,29 @@ function frame() {
       frameEdge = frameThicc;
     }
     fill(frameCol);
-    rect(0, -height/2, width, frameEdge); //top
-    rect(0, height/2, width, frameEdge); //bot
-    rect(-width/2, 0, frameEdge, height); //left
-    rect(width/2, 0, frameEdge, height); //right
+    rect(0, -height/2, width + excess, frameEdge); //top
+    rect(0, height/2, width + excess, frameEdge); //bot
+    rect(-width/2, 0, frameEdge, height + excess); //left
+    rect(width/2, 0, frameEdge, height + excess); //right
   }
 }
 
-function imagetry(words, drum, counter) {
-  let eyeType; 
-  let mouthType;
+function imagetry(words, drum, counter, eyetype) {
   let faceSize = 800;
+  let faceOffset = 30;
+  let mouthType;
 
-  //mouth stuff, opend mouth when words are said
+  //mouth stuff, opens mouth when words are said
   if (words == ""){
     mouthType = mouthClos;
   } else {
     mouthType = mouthOpe;
   }
   
-  //makes eyes normal and closes mouth when chorus plays since words dont sync
+  //closes mouth when chorus plays since words dont sync
   if (counter <= chor1[1] || counter >= chor2[0] && counter <= chor2[1] || counter >= chor3[0]) {
-    eyeType = eyeNorm;
     mouthType = mouthClos;
-  } else {
-    eyeType = eyeAng;
-  }
+  } 
   image(mouthType, 0, faceOffset, faceSize, faceSize);
 
   //eye stuff
@@ -258,10 +327,10 @@ function imagetry(words, drum, counter) {
   rect(-110, faceOffset - 120, 45, 50); //left pupil
   rect(110, faceOffset - 120, 45, 50); //right pupil
 
-  image(eyeType, 0, 30, faceSize, faceSize); //left eye
+  image(eyetype, 0, 30, faceSize, faceSize); //left eye
   push();
-  scale(-1, 1);
-  image(eyeType, 0, 30, faceSize, faceSize); //right eye
+  scale(-1, 1); //flips eye image
+  image(eyetype, 0, 30, faceSize, faceSize); //right eye
   pop();
 }
 
